7177eca8dd860fdadf19f3515f8f075eaae18403
Ahyoung Lim <72844788+ahyoung-lim@users.noreply.github.com>
2024-06-10 17:51:55 +0100

    Update All-Action.yaml

--- scraper/All-job.py
+++ scraper/All-job.py
@@ -35,111 +35,104 @@ formatted_date = date_string.strftime("%Y-%m-%d")
 print(f"Extracted Date: {formatted_date}")
 
 # Create a new DataFrame with today's date and the extracted report date
-today = date.today()
-table = [{'Sys_date': today.strftime('%Y-%m-%d'), 'Report_date': formatted_date}]
-table = pd.DataFrame(table)
-print(table)
+now = datetime.now() # current date and time
+
+table = [{'Sys_date': now.strftime('%Y-%m-%d %H:%M'), 'Report_date': formatted_date}]
+df_current = pd.DataFrame(table)
+print(df_current)
 
 
 # Append the new data to the existing CSV file
-df_current = table
 
-path = Path("data/report_date.csv")
+# if the file already exists, save it to a dataframe and then append to a new one    
+df_main_old = pd.read_csv("https://raw.githubusercontent.com/ahyoung-lim/DengueCrawler/main/data/report_date.csv")
+df_main_new = pd.concat([df_main_old, df_current])
 
-if path.is_file() == False:
-  # if false, save initial main file  
-  df_current.to_csv("data/report_date.csv", index = False)
-  print("Save initial file")
-else:
-  # if the file already exists, save it to a dataframe and then append to a new one    
-  df_main_old = pd.read_csv("https://raw.githubusercontent.com/ahyoung-lim/DengueCrawler/main/data/report_date.csv")
-  df_main_new = pd.concat([df_main_old, df_current])
+# save to dataframe and overwrite the old file
+df_main_new.to_csv("data/report_date.csv", index = False)
 
-  # save to dataframe and overwrite the old file
-  df_main_new.to_csv("data/report_date.csv", index = False)
+# Read the last date from the CSV file
+df_main_new['Sys_date'] = pd.to_datetime(df_main_new['Sys_date'], format='%Y-%m-%d %H:%M')
+df_main_new['Report_date'] = pd.to_datetime(df_main_new['Report_date'], format='%Y-%m-%d')
 
-  # Read the last date from the CSV file
-  df_main_new['Sys_date'] = pd.to_datetime(df_main_new['Sys_date'], format='%Y-%m-%d')
-  df_main_new['Report_date'] = pd.to_datetime(df_main_new['Report_date'], format='%Y-%m-%d')
+df_main_new = df_main_new.sort_values(by='Sys_date', ascending=False)
 
-  df_main_new = df_main_new.sort_values(by='Sys_date', ascending=False)
+last_report_date = df_main_new['Report_date'].iloc[0]
+second_last_date = df_main_new['Report_date'].iloc[1]
 
-  last_report_date = df_main_new['Report_date'].iloc[0]
-  second_last_date = df_main_new['Report_date'].iloc[1]
+if last_report_date == second_last_date: 
+   print("No data updates")
 
-  if last_report_date == second_last_date: 
-     print("No data updates")
+# If the date has been updated then run Selenium and download data
+else: 
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    import time
+    
+    chrome_options = Options()    
+    options = [
+    # Define window size here
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+    ]
 
-  # If the date has been updated then run Selenium and download data
-  else: 
-        from selenium import webdriver
-        from selenium.webdriver.common.keys import Keys
-        from selenium.webdriver.common.by import By
-        from selenium.webdriver.support.ui import WebDriverWait
-        from selenium.webdriver.support import expected_conditions as EC
-        from selenium.webdriver.chrome.options import Options
-        from selenium.webdriver.chrome.service import Service
-        import time
+    for option in options:
+        chrome_options.add_argument(option)
+
+    # Set the download directory to the GitHub repository folder
+    github_workspace = os.getenv('GITHUB_WORKSPACE')
+    download_directory = os.path.join(github_workspace, 'Downloads')
+
+    prefs = {"download.default_directory": download_directory,}
         
-        chrome_options = Options()    
-        options = [
-        # Define window size here
-        "--headless",
-        "--disable-gpu",
-        "--window-size=1920,1200",
-        "--ignore-certificate-errors",
-        "--disable-extensions",
-        "--no-sandbox",
-        "--disable-dev-shm-usage"
-        ]
+    chrome_options.add_experimental_option("prefs", prefs)
 
-        for option in options:
-            chrome_options.add_argument(option)
+    driver = webdriver.Chrome(service=Service(), options = chrome_options)
 
-        # Set the download directory to the GitHub repository folder
-        github_workspace = os.getenv('GITHUB_WORKSPACE')
-        download_directory = os.path.join(github_workspace, 'Downloads')
+    driver.get('https://worldhealthorg.shinyapps.io/dengue_global/')
+    print(driver.title)
+    # with open('./GitHub_Action_Results.txt', 'w') as f:
+    #     f.write(f"This was written with a GitHub action {driver.title}")
 
-        prefs = {"download.default_directory": download_directory,}
-            
-        chrome_options.add_experimental_option("prefs", prefs)
+    # Wait for the "I accept" button to be clickable and then click it
+    accept_button = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "closeModal"))
+    )
+    driver.execute_script("arguments[0].scrollIntoView();", accept_button)
+    driver.execute_script("arguments[0].click();", accept_button)
 
-        driver = webdriver.Chrome(service=Service(), options = chrome_options)
-
-        driver.get('https://worldhealthorg.shinyapps.io/dengue_global/')
-        print(driver.title)
-        # with open('./GitHub_Action_Results.txt', 'w') as f:
-        #     f.write(f"This was written with a GitHub action {driver.title}")
-
-        # Wait for the "I accept" button to be clickable and then click it
-        accept_button = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.ID, "closeModal"))
-        )
-        driver.execute_script("arguments[0].scrollIntoView();", accept_button)
-        driver.execute_script("arguments[0].click();", accept_button)
-
-        #accept_button.click()
+    #accept_button.click()
 
 
-        # Find and click the "Download Data" link in the menu
-        download_link = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[@data-value='dl_data']"))
-        )
-        driver.execute_script("arguments[0].scrollIntoView();", download_link)
-        driver.execute_script("arguments[0].click();", download_link)
+    # Find and click the "Download Data" link in the menu
+    download_link = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[@data-value='dl_data']"))
+    )
+    driver.execute_script("arguments[0].scrollIntoView();", download_link)
+    driver.execute_script("arguments[0].click();", download_link)
 
-        time.sleep(5)
+    time.sleep(5)
 
-        # Click the button to download all data
-        download_all_data_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "dl_all_data"))
-        )
-        driver.execute_script("arguments[0].scrollIntoView();", download_all_data_button)
-        driver.execute_script("arguments[0].click();", download_all_data_button)
+    # Click the button to download all data
+    download_all_data_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "dl_all_data"))
+    )
+    driver.execute_script("arguments[0].scrollIntoView();", download_all_data_button)
+    driver.execute_script("arguments[0].click();", download_all_data_button)
 
-        time.sleep(5)
+    time.sleep(5)
 
-        driver.quit()
+    driver.quit()
 
 
 
